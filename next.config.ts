80f0226d702bd293d7a366bfc30a5e771066136c
Hoàng Anh Tú <tuhoang.121299@gmail.com>
2025-06-20 15:20:20 +0700

    init source

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
+import initializeBundleAnalyzer from '@next/bundle-analyzer';
+
+// https://www.npmjs.com/package/@next/bundle-analyzer
+const withBundleAnalyzer = initializeBundleAnalyzer({
+    enabled: process.env.BUNDLE_ANALYZER_ENABLED === 'true'
+});
+
+// https://nextjs.org/docs/pages/api-reference/next-config-js
 const nextConfig: NextConfig = {
-  /* config options here */
+    output: 'standalone'
 };
 
-export default nextConfig;
+// Merge the plugins
+const compose =
+    (...fns: Function[]) =>
+    (config: NextConfig) =>
+        fns.reduceRight((acc, fn) => fn(acc), config);
+
+export default compose(withBundleAnalyzer)(nextConfig);
